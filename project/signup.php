b45d366df44a5b5637d2f391d5d505b5ae3f71a0
Wojak1n <juraya30@gmail.com>
2025-06-02 10:22:16 +0100

    Login fix

--- project/signup.php
+++ project/signup.php
@@ -72,9 +72,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $error = implode('<br>', $errors);
     }
 }
-
-// Include header
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -128,7 +125,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <h1 class="text-3xl font-bold text-center text-gray-900 mb-8">Create an Account</h1>
+    <div class="text-center mb-1">
+            <h1 class="text-3xl font-bold text-green-800 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-2" width="24" height="24" viewBox="0 0 512 512" <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#266333" d="M272 96c-78.6 0-145.1 51.5-167.7 122.5c33.6-17 71.5-26.5 111.7-26.5l88 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-16 0-72 0s0 0 0 0c-16.6 0-32.7 1.9-48.3 5.4c-25.9 5.9-49.9 16.4-71.4 30.7c0 0 0 0 0 0C38.3 298.8 0 364.9 0 440l0 16c0 13.3 10.7 24 24 24s24-10.7 24-24l0-16c0-48.7 20.7-92.5 53.8-123.2C121.6 392.3 190.3 448 272 448l1 0c132.1-.7 239-130.9 239-291.4c0-42.6-7.5-83.1-21.1-119.6c-2.6-6.9-12.7-6.6-16.2-.1C455.9 72.1 418.7 96 376 96L272 96z"/></svg>
+                Create An Account
+            </h1>
+        </div>
     </div>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
